0a001d985386ab46744d03bdd264346f1248d3e0
HarisAurangzeb <ha23ahb@herts.ac.uk>
2025-12-10 15:40:26 -0800

    Clean Fatal Column

--- Haris/SharkAttack.R
+++ Haris/SharkAttack.R
@@ -3,3 +3,8 @@
 shark_data$Sex <- toupper(trimws(shark_data$Sex))
 shark_data$Sex[shark_data$Sex %in% c("MALE", "M")] <- "M"
 shark_data$Sex[shark_data$Sex %in% c("FEMALE", "F")] <- "F"
+# 4. Clean Fatal column
+# ------------------------------
+shark_data$Fatal <- toupper(trimws(shark_data$Fatal))
+shark_data$Fatal[shark_data$Fatal %in% c("Y", "YES", "FATAL")] <- "Y"
+shark_data$Fatal[shark_data$Fatal %in% c("N", "NO")] <- "N"
