b0a83dc390ee058a051387bb60a7b79e37c96ce1
br23uk <br23abb@herts.ac.uk>
2025-12-10 21:07:46 +0000

    Inspecting column names

--- BaqarRaza/sharkattack.r
+++ BaqarRaza/sharkattack.r
@@ -10,3 +10,14 @@ library(ggplot2)
 # ------------------------------
 shark_data <- read.csv("D:attacks.csv",
                        stringsAsFactors = FALSE)
+# ------------------------------
+# 2. Inspect column names
+# ------------------------------
+colnames(shark_data)  # Check exact column names
+
+# Find Fatal column (in case of hidden spaces)
+fatal_col <- grep("Fatal", colnames(shark_data), value = TRUE)
+print(fatal_col)  # Just to confirm column name
+
+# Rename Fatal column to simple name
+names(shark_data)[grep("Fatal", names(shark_data))] <- "Fatal"
