ea0b598dc5c379c69c867cf551779684ee216fb8
br23uk <br23abb@herts.ac.uk>
2025-12-11 15:00:38 +0000

    Summary table

--- BaqarRaza/sharkattack.r
+++ BaqarRaza/sharkattack.r
@@ -21,3 +21,12 @@ print(fatal_col)  # Just to confirm column name
 
 # Rename Fatal column to simple name
 names(shark_data)[grep("Fatal", names(shark_data))] <- "Fatal"
+
+
+# 6. Summary tables
+# ------------------------------
+summary_table <- table(shark_clean$Sex, shark_clean$Fatal)
+prop_table <- prop.table(summary_table, 1)
+
+print(summary_table)
+print(round(prop_table,3))  # Proportions rounded to 3 decimals
