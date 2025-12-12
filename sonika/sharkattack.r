2661f19a504759555980ca7997c86b5e9ee0f264
Sn23aey <Sn23aey@herts.ac.uk>
2025-12-12 00:22:29 +0000

    Chi square test of independence

--- sonika/sharkattack.r
+++ sonika/sharkattack.r
@@ -1,4 +1,11 @@
 # 8. Mosaic plot of raw counts
 # ------------------------------
 mosaicplot(summary_table, main="Shark Attack Fatalities by Sex",
-           color=c("lightblue","red"))
+           color=c("lightblue","red"))   
+# 9. Chi-square test of independence
+# ==============================
+
+# Create a contingency table of Sex vs Fatal
+sex_fatal_table <- table(shark_clean$Sex, shark_clean$Fatal)
+print("Contingency Table:")
+print(sex_fatal_table)  
